60e12469dc62c7da70e449330ebeb196d81621e1
kalpana <kalpanaudara058@gmail.com>
2026-02-24 21:02:14 +0530

    Installed prettier code formatter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter, Route, Routes} from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/test" element={<App />} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
